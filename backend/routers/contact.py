644faf95f6da67c294be2d3b7b3c4f8609d3e035
Roberto David Morales Ramos <davld7@outlook.com>
2023-11-29 14:58:48 -0600

    environment variables

--- backend/routers/contact.py
+++ backend/routers/contact.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from backend.models.contact import FormData
+from environment import variables
 
 router = APIRouter(prefix="/contact", tags=["contact"])
 
@@ -12,4 +13,6 @@ async def submit_form(form_data: FormData):
     print(form_data.email)
     print(form_data.subject)
     print(form_data.message)
+    print(variables.sender_email)
+    print(variables.sender_password)
     return JSONResponse(content={"message": "Form submitted successfully"})
---
+++ environment.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import ValidationError
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8')
+
+    def __init__(self):
+        try:
+            super().__init__()
+        except ValidationError as exception:
+            print(f"{str(exception)}")
+            exit(1)
+
+
+class Variables(Settings):
+    sender_email: str
+    sender_password: str
+
+
+@lru_cache
+def get_variables() -> Variables:
+    return Variables()
+
+
+variables = get_variables()
