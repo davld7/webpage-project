0340a15521d7c0fb3625a3a4f30f613748c765a1
Roberto David Morales Ramos <davld7@outlook.com>
2023-11-30 14:24:08 -0600

    added social networks

---
+++ js/social.js
@@ -0,0 +1,24 @@
+function openInstagram(event) {
+
+    event.preventDefault();
+
+    window.open('https://www.instagram.com/netflixlat/?hl=es-la', '_blank').focus();
+}
+function openFacebook(event) {
+
+    event.preventDefault();
+
+    window.open('https://www.facebook.com/NetflixLatino/?locale=es_LA', '_blank').focus();
+}
+function openTwitter(event) {
+
+    event.preventDefault();
+
+    window.open('https://twitter.com/netflixlat?lang=es', '_blank').focus();
+}
+function openLinkedin(event) {
+
+    event.preventDefault();
+
+    window.open('https://es.linkedin.com/company/netflix', '_blank').focus();
+}
