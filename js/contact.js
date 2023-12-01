034eab3c5772260684d3d4a624b7aa1f7b9a2234
Roberto David Morales Ramos <davld7@outlook.com>
2023-11-30 18:11:31 -0600

    updated contact script

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,43 @@
 /**
  * Submits the form data to the server.
  *
- * @return {Promise} A promise that resolves when the form data is successfully submitted.
+ * @return {Promise} A promise that resolves when the form data is 
+ * successfully submitted.
  */
 async function submitForm() {
-    
-    const formData = {
-        first_name: document.getElementById("first_name").value,
-        last_name: document.getElementById("last_name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
-    };
+    const fields = ['first_name', 'last_name', 'email', 
+                    'subject', 'message'];
+    const formData = {};
+
+    for (const field of fields) {
+        formData[field] = document.getElementById(field).value;
+        if (!formData[field]) {
+            document.getElementById("formStatus").textContent =
+                "Por favor, complete todos los campos antes de enviar.";
+            return;
+        }
+    }
 
     try {
-        const response = await fetch('https://netflix.zeabur.app/contact/submit-form', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
+        const response = await fetch(
+            'https://netflix.zeabur.app/contact/submit-form', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(formData),
+            }
+        );
+        if (!response.ok) throw new Error('Network response was not ok.');
+
         const data = await response.json();
         console.log('Success:', data);
+
+        fields.forEach(field => document.getElementById(field).value = '');
+        document.getElementById("formStatus").textContent =
+            "¡Formulario enviado con éxito!";
     } catch (error) {
         console.error('Error:', error);
+        document.getElementById("formStatus").textContent =
+            "No se ha podido enviar el formulario. " +
+            "Vuelva a intentarlo más tarde.";
     }
 }
